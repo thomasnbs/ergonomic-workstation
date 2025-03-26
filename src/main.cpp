6b6e20e12a5c745ea0248bcb502e0e26ffa14fad
thomasnbs <thomas.nobis.pro@gmail.com>
2025-03-26 08:30:10 +0100

    commit initial

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,31 @@
 #include <Arduino.h>
+#define BOUTONSW 18 //BROCHE A DEFINIR
+#define LEDALLUMER 16 //BROCHE A DEFINIR
+#define LEDALLUMER2 17
 
-// put function declarations here:
-int myFunction(int, int);
+#include <Wire.h>
+
+#define adresseBaseDesBacs 0x20
+#define numeroDuBac_1 0
+#define numeroDuBac_2 1
+#define numeroDuBac_3 2
+
+#define ATTENDRE_1_SECONDE 1000
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+ Wire.begin();
+
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  Wire.beginTransmission(adresseBaseDesBacs + numeroDuBac_1); // Début de la transmission à l'adresse 0x20
+  Wire.write(0b11111110);                             // 
+  Wire.endTransmission();
+  delay(ATTENDRE_1_SECONDE);
+  Wire.beginTransmission(adresseBaseDesBacs + numeroDuBac_1); // Début de la transmission à l'adresse 0x20
+  Wire.write(0b11111111);                             // 
+  Wire.endTransmission();
+  delay(ATTENDRE_1_SECONDE);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
